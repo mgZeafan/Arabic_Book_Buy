380b7441dacaa5e339f4a7079aa5a26e52ddae71
mohamed gomaa <zeafanm@gmail.com>
2017-11-08 01:26:51 +0200

    Initial commit

---
+++ app/src/main/java/com/app/mohamedgomaa/arabic_book_buy/CheckConnection_Internet.java
@@ -0,0 +1,27 @@
+package com.app.mohamedgomaa.arabic_book_buy;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+ class CheckConnection_Internet{
+    Context context;
+    ConnectivityManager ConMng;
+
+     public CheckConnection_Internet(Context context) {
+        this.context = context;
+    }
+    public boolean IsConnection()
+    {
+        ConMng = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (ConMng != null){
+            NetworkInfo info=ConMng.getActiveNetworkInfo();
+            if(info!=null && info.isConnected())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/app/mohamedgomaa/arabic_book_buy/Splash_Layer.java
@@ -0,0 +1,32 @@
+package com.app.mohamedgomaa.arabic_book_buy;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash_Layer extends AppCompatActivity {
+    Animation anim;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__layer);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(2000);
+                        startActivity(new Intent(Splash_Layer.this, MainActivity.class));
+                        finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
