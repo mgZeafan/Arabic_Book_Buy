537d9a947df33027300b1acacf030bb2e2d31f6d
mohamed <zeafanm@gmail.com>
2017-11-17 23:33:59 +0200

    11:33 يوم الجمعه

--- app/src/main/java/com/app/mohamedgomaa/arabic_book_buy/ItemAdapter.java
+++ app/src/main/java/com/app/mohamedgomaa/arabic_book_buy/ItemAdapter.java
@@ -1,36 +1,81 @@
 package com.app.mohamedgomaa.arabic_book_buy;
 
+import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.widget.CheckBox;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * Created by mohamed on 11/15/2017.
- */
+import com.borjabravo.readmoretextview.ReadMoreTextView;
+import com.squareup.picasso.Picasso;
 
-public class ItemAdapter extends  RecyclerView.Adapter<ItemAdapter.Holder> implements View.OnLongClickListener{ {
+import java.util.ArrayList;
+import java.util.Locale;
 
-    public static class Holder extends RecyclerView.ViewHolder
-    {
-        ImageView img;
-        TextView txtname;
-        TextView txtemail;
-        CheckBox box;
-        CardView cardView;
-        MainActivity mainActivity;
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>{
+    private ArrayList<item> myList;
+    private Context myCon;
 
-        public Holder(View view, final MainActivity mainActivity) {
-            super(view);
-            img=(ImageView)view.findViewById(R.id.imageView123);
-            txtemail=(TextView)view.findViewById(R.id.email);
-            txtname=(TextView)view.findViewById(R.id.name);
-            box=(CheckBox)view.findViewById(R.id.checkbox);
-            cardView=(CardView)view.findViewById(R.id.card_view);
-            this.mainActivity=mainActivity;
-            cardView.setOnLongClickListener(mainActivity);
-        }
+    public ItemAdapter(ArrayList<item> myList, Context myCon) {
+        this.myList = myList;
+        this.myCon = myCon;
     }
 
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_card, parent, false);
+        Holder holder = new Holder(v);
+        return holder;
+    }
+
+    static int count = 0;
+
+    @Override
+    public void onBindViewHolder(Holder holder, final int position) {
+        Picasso.with(myCon).load(myList.get(position).pth_photo).placeholder(R.drawable.file_wait).error(R.drawable.file_wait).into(holder.img);
+        holder.txtPrice.setText(holder.txtPrice.getText()+ String.valueOf(myList.get(position).price));
+        if(Locale.getDefault().getLanguage().equals("ar")) {
+            holder.txtDetails.setText(myList.get(position).details_ar);
+            holder.txtTitle.setText(myList.get(position).title_ar);
+            holder.txtAuthor.setText(myList.get(position).author_ar);
+        }else {
+            holder.txtDetails.setText(myList.get(position).details_en);
+            holder.txtTitle.setText(myList.get(position).title_en);
+            holder.txtAuthor.setText(myList.get(position).author_en);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return myList.size();
+    }
+
+    public static class Holder extends RecyclerView.ViewHolder {
+        ImageView img;
+        TextView txtTitle;
+        com.borjabravo.readmoretextview.ReadMoreTextView txtDetails;
+        TextView txtPrice;
+        TextView txtAuthor;
+        Button dwn;
+        Button rvw;
+        CardView cardView;
+
+        public Holder(View view) {
+            super(view);
+            img = (ImageView) view.findViewById(R.id.imgbook);
+            txtTitle = (TextView) view.findViewById(R.id.titleName);
+            txtDetails = (ReadMoreTextView) view.findViewById(R.id.details);
+            txtPrice = (TextView) view.findViewById(R.id.price);
+            txtAuthor=(TextView)view.findViewById(R.id.author);
+            dwn=(Button)view.findViewById(R.id.download);
+            rvw=(Button)view.findViewById(R.id.review);
+            cardView=(CardView)view.findViewById(R.id.card_view);
+        }
+
+    }
 }
--- app/src/main/java/com/app/mohamedgomaa/arabic_book_buy/MainActivity.java
+++ app/src/main/java/com/app/mohamedgomaa/arabic_book_buy/MainActivity.java
@@ -5,20 +5,64 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    final String ParentGuid="ListBook";
+    ItemAdapter itemAdapter;
+    ArrayList<item> Books=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        recyclerView = (RecyclerView) findViewById(R.id.m_RecyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.recycleView);
         recyclerView.setHasFixedSize(true);
         GridLayoutManager gridLayoutManager=new GridLayoutManager(this,1);
         recyclerView.setLayoutManager(gridLayoutManager);
-        recyclerView_dAdapter = new RecyclerViewAdapter(, MainActivity.this);
-        recyclerView.setAdapter(recyclerView_dAdapter);
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-
-        }
+        Iniliazation();
+        AddEventListener();
+        itemAdapter = new ItemAdapter(Books, MainActivity.this);
+        recyclerView.setAdapter(itemAdapter);
+        itemAdapter.notifyDataSetChanged();
 }
+
+    private void AddEventListener() {
+        databaseReference.child(ParentGuid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+            if(Books.size()>0)
+            {
+                Books.clear();
+            }
+            for (DataSnapshot PostSnapshot:dataSnapshot.getChildren())
+            {
+                item it=PostSnapshot.getValue(item.class);
+                Books.add(it);
+            }
+                itemAdapter = new ItemAdapter(Books, MainActivity.this);
+                recyclerView.setAdapter(itemAdapter);
+                itemAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void Iniliazation() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference();
+    }
+    }
